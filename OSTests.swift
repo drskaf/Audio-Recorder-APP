b07689692dc2cecb6cc30ad4b74a87e4094250ee
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2024-01-09 10:14:44 +0000

    Update OSTests.swift

--- OSTests.swift
+++ OSTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import OS
 
-final class OSTests: XCTestCase {
+final class OSTests: XCTestCase {   
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
