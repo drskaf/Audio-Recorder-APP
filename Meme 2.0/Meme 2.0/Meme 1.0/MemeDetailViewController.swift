1c43dd3e93cd6940af6e87f23365ca6480d70ed3
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2024-03-29 23:52:17 +0000

    Add files via upload

---
+++ Meme 2.0/Meme 2.0/Meme 1.0/MemeDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MemeDetailViewController.swift
+//  Meme 1.0
+//
+//  Created by Ebraham Alskaf on 26/03/2024.
+//
+
+import UIKit
+
+
+
+class MemeDetailViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    var meme: Meme!
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
+    // MARK: Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.imageview.image = self.meme.memedImage
+        
+        // Hide the tab bar when this view appears
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the tab bar when this view disappears
+        self.tabBarController?.tabBar.isHidden = false
+    }
+}
+
