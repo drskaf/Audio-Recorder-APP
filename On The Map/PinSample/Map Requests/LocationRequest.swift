0686c5bf934c8a7786f2462b608ddc84f255317f
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2024-06-10 13:00:34 +0100

    Add files via upload

---
+++ On The Map/PinSample/Map Requests/LocationRequest.swift
@@ -0,0 +1,14 @@
+//
+//  LocationRequest.swift
+//  PinSample
+//
+//  Created by Ebraham Alskaf on 07/06/2024.
+//  Copyright © 2024 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct LocationRequest: Codable {
+    let objectId: String
+
+}
